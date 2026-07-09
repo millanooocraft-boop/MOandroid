417f1735ee7932fc96702e54bae9faa07b6d13e7
millanooocraft-boop <millanooocraft@gmail.com>
2026-07-09 22:18:16 +0100

    Update MainActivity.kt

--- app/src/main/java/com/mo/assistant/MainActivity.kt
+++ app/src/main/java/com/mo/assistant/MainActivity.kt
@@ -6,19 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.navigation.compose.rememberNavController
 import com.mo.assistant.ui.MoApp
 import com.mo.assistant.ui.theme.MOTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         setContent {
             MOTheme {
                 Surface(
